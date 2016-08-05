4e4c0ae8bb5090fd85b401bfda6a41b3a08a73ae
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-05 14:08:33 -0700

    add some view structure to index and add getbandname() to performance, passing performanceT6

--- Objects/Performance.cs
+++ Objects/Performance.cs
@@ -102,7 +102,34 @@ namespace BandTracker
 
     public string GetBandName()
     {
-      return "Hello";
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT name FROM bands WHERE id = @BandId;", conn);
+      SqlParameter bandIdParameter = new SqlParameter();
+      bandIdParameter.ParameterName = "@BandId";
+      bandIdParameter.Value = this.GetBandId();
+
+      cmd.Parameters.Add(bandIdParameter);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      string foundBandName = "";
+
+      while (rdr.Read())
+      {
+        foundBandName = rdr.GetString(0);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundBandName;
     }
 
     public void Save()
