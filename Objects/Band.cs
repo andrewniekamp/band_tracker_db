db63b1c2abb3c9df2f297db297aba46e3b85f651
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-05 09:36:31 -0700

    add band class with constructor, get/set methods and bandT1 for confirm DB empty

---
+++ Objects/Band.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace BandTracker
+{
+  public class Band
+  {
+    private int _id;
+    private string _name;
+
+    public Band(string name, int id = 0)
+    {
+      _id = id;
+      _name = name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+  }
+}
