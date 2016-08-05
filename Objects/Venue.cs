5a4ea830c210036675182b977ae1ab591070f8ff
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-05 09:01:14 -0700

    add venueT1 and venue object, constructor, getall

---
+++ Objects/Venue.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace BandTracker
+{
+  public class Venue
+  {
+    private int _id;
+    private string _name;
+
+    public Venue(string Name, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+    }
+
+    public static List<Venue> GetAll()
+    {
+      List<Venue> allVenues = new List<Venue>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM venues;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while (rdr.Read())
+      {
+        int venueId = rdr.GetInt32(0);
+        string venueName = rdr.GetString(1);
+
+        Venue newVenue = new Venue(venueName, venueId);
+        allVenues.Add(newVenue);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return allVenues;
+    }
+  }
+}
---
+++ Tests/VenueTest.cs
@@ -0,0 +1,27 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+  public class VenueTest : IDisposable
+  {
+    public VenueTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+    }
+    public void Dispose()
+    {
+      Venue.DeleteAll();
+    }
+
+    [Fact]
+    public void T1_DBEmptyAtFirst()
+    {
+      int result = Venue.GetAll().Count;
+      Assert.Equal(0, result);
+    }
+  }
+}
