694aaecb499856a03e75b3ced2612507925f0cc3
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-05 09:56:59 -0700

    add performanceT1 to confirm DB is empty at first

---
+++ Tests/PerformanceTest.cs
@@ -0,0 +1,27 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+  public class PerformanceTest : IDisposable
+  {
+    public PerformanceTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+    }
+    public void Dispose()
+    {
+      Performance.DeleteAll();
+    }
+
+    [Fact]
+    public void T1_DBEmptyAtFirst()
+    {
+      int result = Performance.GetAll().Count;
+      Assert.Equal(0, result);
+    }
+  }
+}
