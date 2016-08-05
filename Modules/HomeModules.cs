edcfc4c47b21bd81e0864549d512ef40402c4862
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-05 11:13:41 -0700

    add routes to add band and venue to direct to index

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -17,9 +17,27 @@ namespace BandTracker
         return View["index.cshtml", model];
       };
 
-      // Post["/success"] = _ => {
-      //
-      // };
+      Post["/band_added"] = _ => {
+        Band newBand = new Band(Request.Form["band-name"]);
+        newBand.Save();
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        List<Venue> allVenues = Venue.GetAll();
+        List<Band> allBands = Band.GetAll();
+        model.Add("venues", allVenues);
+        model.Add("bands", allBands);
+        return View["index.cshtml", model];
+      };
+
+      Post["/venue_added"] = _ => {
+        Venue newVenue = new Venue(Request.Form["venue-name"]);
+        newVenue.Save();
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        List<Venue> allVenues = Venue.GetAll();
+        List<Band> allBands = Band.GetAll();
+        model.Add("venues", allVenues);
+        model.Add("bands", allBands);
+        return View["index.cshtml", model];
+      };
     }
   }
 }
