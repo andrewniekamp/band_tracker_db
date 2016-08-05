dff1833396bbe4509caa3f5a8a13e2b41b4a47b7
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-05 09:54:07 -0700

    add performance object intended to store band appearances at venues

---
+++ Objects/Performance.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace BandTracker
+{
+  public class Performance
+  {
+    private int _id;
+    private int _venueId;
+    private int _bandId;
+    private DateTime _performanceDate;
+
+    public Performance (int venueId, int bandId, DateTime performanceDate, int id = 0)
+    {
+      _id = id;
+      _venueId = venueId;
+      _bandId = bandId;
+      _performanceDate = performanceDate;
+    }
+
+  }
+}
