6da37e9dc25ba7e6dc4ebfb2be111370b7f764c1
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-05 10:52:39 -0700

    add route to index that shows all current bands/venues

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace BandTracker
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        List<Venue> allVenues = Venue.GetAll();
+        List<Band> allBands = Band.GetAll();
+        model.Add("venues", allVenues);
+        model.Add("bands", allBands);
+        return View["index.cshtml", model];
+      };
+    }
+  }
+}
